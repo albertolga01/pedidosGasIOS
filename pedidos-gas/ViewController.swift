c180874c2a4b059de783099cc1a553e0cd7e8527
MaryLoera <maria_loeraroman01@hotmail.com>
2023-11-21 17:03:21 -0700

    20231121

--- pedidos-gas/ViewController.swift
+++ pedidos-gas/ViewController.swift
@@ -10,7 +10,7 @@ import WebKit
 
 class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler {
    /*
-    let JSON = """
+    let JSON = """nn
     {
         "telefono":"47093",
         "noConsumidor":"47093,
@@ -21,7 +21,8 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
         "loggeado":"1"
     }
     """
-*/
+
+     
     
     struct BlogPost: Decodable {
         enum Category: String, Decodable {
@@ -37,6 +38,22 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
         var defLoggeado: Int
     }
     
+    */
+       
+    struct Datos: Codable {
+                      
+                    let telefono: String?
+                    let apellidos: String?
+                    let email: String?
+                    let noConsumidor: String?
+                    let nombres: String?
+                    let identificador_externo: String?
+                    let tipo: String?
+                    let loggeado: String?
+                }
+   
+   
+    
     override func loadView() {
         
         var defTelefono = "defTelefono"
@@ -46,13 +63,7 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
         var defEmail = "email"
         var defIdentificador_externo = "idexterno"
         
-        UserDefaults.standard.set(defTelefono, forKey:"defTelefono");
-        UserDefaults.standard.set(defConsumidor, forKey:"defConsumidor");
-        UserDefaults.standard.set(defNombres, forKey:"defNombres");
-        UserDefaults.standard.set(defApellidos, forKey:"defApellidos");
-        UserDefaults.standard.set(defEmail, forKey:"defEmail");
-        UserDefaults.standard.set(defIdentificador_externo, forKey:"defIdentificador_externo");
-        UserDefaults.standard.synchronize();
+      
         
           let webConfiguration = WKWebViewConfiguration()
           let contentController = WKUserContentController()
@@ -79,42 +90,91 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKSc
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        
+        self.reloadView()
+    }
+    
+    func reloadView() {
+        //do your initalisations here
         let url = URL(string:"http://localhost:3000")
         let request = URLRequest(url: url!)
         webView.navigationDelegate = self
         webView.load(request)
-         
     }
-   
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("Resume")
+        webView.reloadFromOrigin()
+            
+        }
+    
+    
+    
     func webView(_ webView: WKWebView,
         didFinish navigation: WKNavigation!) {
         print("loaded")
         
+        if let favExist = UserDefaults.standard.array(forKey: "defTelefono") {
+                
+            var defTelefono = UserDefaults.standard.string(forKey: "defTelefono") as! String
+            let defConsumidor = UserDefaults.standard.string(forKey: "defConsumidor") as! String
+            let defNombres = UserDefaults.standard.string(forKey: "defNombres") as! String
+            let defApellidos = UserDefaults.standard.string(forKey: "defApellidos") as! String
+            let defEmail = UserDefaults.standard.string(forKey: "defEmail") as! String
+            let defIdentificador_externo = UserDefaults.standard.string(forKey: "defIdentificador_externo") as! String
+            
+            self.webView.evaluateJavaScript("{window.reactFunction1('\(defTelefono)','\(defConsumidor)','\(defNombres)','\(defApellidos)','\(defEmail)','\(defIdentificador_externo)');}") { (any, error) in
+            print("Error : \(error)")
+            }
+             
+            }else {
+                
+                return
+            }
         
-        var defTelefono = UserDefaults.standard.string(forKey: "defTelefono") as! String
-        let defConsumidor = UserDefaults.standard.string(forKey: "defConsumidor") as! String
-        let defNombres = UserDefaults.standard.string(forKey: "defNombres") as! String
-        let defApellidos = UserDefaults.standard.string(forKey: "defApellidos") as! String
-        let defEmail = UserDefaults.standard.string(forKey: "defEmail") as! String
-        let defIdentificador_externo = UserDefaults.standard.string(forKey: "defIdentificador_externo") as! String
         
         
-        
-        
-        print(defTelefono)
-        /*
-        self.webView.evaluateJavaScript("{window.reactFunction1('\(defTelefono)','\(defConsumidor)','\(defNombres)','\(defApellidos)','\(defEmail)','\(defIdentificador_externo)');}") { (any, error) in
-        print("Error : \(error)")
-        }*/
       }
     
     // Implement `WKScriptMessageHandler`，handle message which been sent by JavaScript
         func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
             if(message.name != "") {
-                //let jsonData = JSON.data(using: String.Encoding.utf8)!
-                //let blogPost: BlogPost = try! JSONDecoder().decode(BlogPost.self, from: jsonData)
+                
+                            print("JavaScript is sending a message \(message.body)")
+                
+               
+             
+                            let JSON = """
+                                \(message.body)
+                            """
 
-                print("JavaScript is sending a message \(message.body)")
+                            let jsonData = JSON.data(using: .utf8)!
+
+                            // Convert the JSON object to a Video object
+                            let datos: Datos = try! JSONDecoder().decode(Datos.self, from: jsonData)
+                            if(datos.tipo! as String == "1"){
+                             print("guardar")
+                                UserDefaults.standard.set(datos.telefono! as String, forKey:"defTelefono");
+                                UserDefaults.standard.set(datos.noConsumidor! as String, forKey:"defConsumidor");
+                                UserDefaults.standard.set(datos.nombres! as String, forKey:"defNombres");
+                                UserDefaults.standard.set(datos.apellidos! as String, forKey:"defApellidos");
+                                UserDefaults.standard.set(datos.email! as String, forKey:"defEmail");
+                                UserDefaults.standard.set(datos.identificador_externo! as String, forKey:"defIdentificador_externo");
+                                UserDefaults.standard.synchronize();
+                            }else if(datos.tipo! as String == "0"){
+                                print("no guardar")
+                                UserDefaults.standard.removeObject(forKey: "defTelefono")
+                                UserDefaults.standard.removeObject(forKey: "defConsumidor")
+                                UserDefaults.standard.removeObject(forKey: "defNombres")
+                                UserDefaults.standard.removeObject(forKey: "defApellidos")
+                                UserDefaults.standard.removeObject(forKey: "defEmail")
+                                UserDefaults.standard.removeObject(forKey: "defIdentificador_externo")
+                                
+                            }
+                            
+                 
             }
         }
 }
